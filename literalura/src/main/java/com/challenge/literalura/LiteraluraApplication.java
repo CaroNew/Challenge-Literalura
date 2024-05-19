f1d0438af7abbcbabf0ab2cdfa7df672f353117f
CaroNew <125692041+CaroNew@users.noreply.github.com>
2024-05-19 16:35:50 -0300

    record datosLibro agregada

--- literalura/src/main/java/com/challenge/literalura/LiteraluraApplication.java
+++ literalura/src/main/java/com/challenge/literalura/LiteraluraApplication.java
@@ -2,6 +2,9 @@ package com.challenge.literalura;
 
 import com.challenge.literalura.mainclass.MainMenu;
 import com.challenge.literalura.service.ApiRequest;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -13,6 +16,7 @@ import java.net.http.HttpResponse;
 
 @SpringBootApplication
 public class LiteraluraApplication implements CommandLineRunner {
+	static final String RUTE = "https://gutendex.com/books";
 	public static void main(String[] args) {
 		SpringApplication.run(LiteraluraApplication.class, args);
 	}
@@ -22,12 +26,24 @@ public class LiteraluraApplication implements CommandLineRunner {
 	public void run(String... args) throws Exception {
 		//MainMenu menu = new MainMenu();
 		//menu.showMenu();
-		String rute = "http://www.gutendex.com/books/1";
+		String rute = "https://gutendex.com/books/1";
 
 		ApiRequest apiRequest = new ApiRequest();
 		String data = apiRequest.getData(rute);
 		System.out.println("*****************************************\n");
-		System.out.println("data: " + data);
+
+		ObjectMapper objectMapper = new ObjectMapper();
+		JsonNode jsonNode = null;
+		try {
+			jsonNode = objectMapper.readTree(data);
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+
+		System.out.println("data:");
+		System.out.println(jsonNode);
+		System.out.println(jsonNode.getClass().getSimpleName());
+
 		System.out.println("*****************************************\n");
 
 
---
+++ literalura/src/main/java/com/challenge/literalura/models/DatosLibro.java
@@ -0,0 +1,16 @@
+package com.challenge.literalura.models;
+
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import java.util.List;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record DatosLibro(
+        @JsonAlias("title") String titulo,
+        @JsonAlias("authors") List<String> autor,
+        @JsonAlias("languages") String idioma,
+        @JsonAlias("download_count") Integer numeroDeDescargas
+) {
+
+}
