d1c067deda7f0e67eacb0fb9ee4bff315ab4ee01
CaroNew <125692041+CaroNew@users.noreply.github.com>
2024-05-19 11:57:00 -0300

    clases menu y peticion http creadas

--- literalura/src/main/java/com/challenge/literalura/LiteraluraApplication.java
+++ literalura/src/main/java/com/challenge/literalura/LiteraluraApplication.java
@@ -1,12 +1,19 @@
 package com.challenge.literalura;
 
+import com.challenge.literalura.mainclass.MainMenu;
+import com.challenge.literalura.service.ApiRequest;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
 @SpringBootApplication
 public class LiteraluraApplication implements CommandLineRunner {
-
+	static final String RUTE = "https://gutendex.com/books";
 	public static void main(String[] args) {
 		SpringApplication.run(LiteraluraApplication.class, args);
 	}
@@ -14,6 +21,16 @@ public class LiteraluraApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		System.out.println("Hello World");
+		//MainMenu menu = new MainMenu();
+		//menu.showMenu();
+		String rute = "http://www.gutendex.com/books/1";
+
+		ApiRequest apiRequest = new ApiRequest();
+		String data = apiRequest.getData("https://gutendex.com/books/1");
+		System.out.println("*****************************************\n");
+		System.out.println("data: " + apiRequest.getData(rute));
+		System.out.println("*****************************************\n");
+
+
 	}
 }
---
+++ literalura/src/main/java/com/challenge/literalura/mainclass/MainMenu.java
@@ -0,0 +1,15 @@
+package com.challenge.literalura.mainclass;
+
+public class MainMenu {
+
+    public void showMenu() {
+        System.out.println("Welcome to Literalura");
+        System.out.println("1. Add a book");
+        System.out.println("2. Add a user");
+        System.out.println("3. Add a review");
+        System.out.println("4. List all books");
+        System.out.println("5. List all users");
+        System.out.println("6. List all reviews");
+        System.out.println("7. Exit");
+    }
+}
