2076dfe86b0a3fe9e80ddc1061d60886f1dac4ed
CaroNew <125692041+CaroNew@users.noreply.github.com>
2024-05-20 22:42:59 -0300

    busqueda de libros hecha

--- literalura/src/main/java/com/challenge/literalura/mainclass/MainMenu.java
+++ literalura/src/main/java/com/challenge/literalura/mainclass/MainMenu.java
@@ -1,16 +1,26 @@
 package com.challenge.literalura.mainclass;
 
+import com.challenge.literalura.models.DatosLibro;
+import com.challenge.literalura.models.DatosLibros;
+import com.challenge.literalura.service.ApiRequest;
+import com.challenge.literalura.service.DataConversion;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.util.List;
 import java.util.Scanner;
 
 public class MainMenu {
-    private Scanner scanner = new Scanner(System.in);
+    private Scanner keyBoard = new Scanner(System.in);
+    private final String BASE_URL = "https://gutendex.com/books";
 
 
     public void showMenu() {
         int option = 0;
         do {
             printMenu();
-            option = scanner.nextInt();
+            option = keyBoard.nextInt();
+            keyBoard.nextLine();
             switch (option) {
                 case 1:
                     searchABookByTitle();
@@ -56,6 +66,29 @@ public class MainMenu {
     }
 
     public void searchABookByTitle() {
-        System.out.println("Buscar y agregar un libro");
+        DataConversion dataobj = new DataConversion();
+        System.out.println("Introduce el titulo del libro a buscar: ");
+        var title = keyBoard.nextLine();
+        var url = BASE_URL + "/?search=" + title.replace(" ", "+");
+        ///?search=quijote+de+la+mancha
+        //System.out.println(url);
+        ApiRequest request = new ApiRequest();
+        String data = request.getData(url);
+        //System.out.println(data);
+        DataConversion dataConversion = new DataConversion();
+        DatosLibros libros = dataConversion.convertData(data, DatosLibros.class);
+        //System.out.println("es vacia: " + libros.libros().isEmpty());
+
+        if(!libros.libros().isEmpty()) {
+            DatosLibro libro = libros.libros().get(0);
+            System.out.println("titulo: " + libro.titulo() +
+                    " idioma: " + libro.idioma() +
+                    " autor: " + libro.autor());
+
+        }else{
+            System.out.println("No se encontraron resultados");
+        }
+
+
     }
 }
